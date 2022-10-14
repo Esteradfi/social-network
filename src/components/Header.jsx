9e5075651a0375ca988a1b37a6a7cd43ea59da95
Ivan Esteradfi <lyskov.ivan.and@gmail.com>
2022-10-14 18:59:08 +0300

    feat: add page structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img alt="logo" src="https://w7.pngwing.com/pngs/220/383/png-transparent-golf-course-logo-royal-putting-greens-creative-travel-logo-free-logo-design-template-photography-logo.png"/>
+        </header>
+    );
+}
+
+export default Header;
